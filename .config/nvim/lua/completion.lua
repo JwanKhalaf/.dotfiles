f85021d43b8264201c9f0508fb284b29c6802bad
Jwan Khalaf <jwan.khalaf@outlook.com>
2021-10-27 22:59:47 +0100

    add completion for more than one lsp server

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -50,6 +50,16 @@
   -- Setup lspconfig.
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-  require('lspconfig')['tsserver'].setup {
-    capabilities = capabilities
-  }
+  local nvim_lsp = require 'lspconfig'
+
+  -- enable the following language servers
+  local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+  for _, lsp in ipairs(servers) do
+    nvim_lsp[lsp].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+  end
+  -- require('lspconfig')['tsserver'].setup {
+  --   capabilities = capabilities
+  -- }
