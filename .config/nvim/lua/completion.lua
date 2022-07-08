bdca56d27a53130065de2c9803ae7b315f6d8dce
Jwan Khalaf <jwan.khalaf@outlook.com>
2022-07-08 23:22:45 +0100

    add elmls server to lspconfig and set things up correctly for rider terminal

--- .config/nvim/lua/completion.lua
+++ .config/nvim/lua/completion.lua
@@ -87,7 +87,7 @@
 
   -- use a loop to conveniently call 'setup' on multiple servers and
   -- map buffer local keybindings when the language server attaches
-  local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
+  local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'elmls' }
   for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
       on_attach = on_attach,
